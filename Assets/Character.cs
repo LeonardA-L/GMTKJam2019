2990b2d00d38e79ca30fc39f271ce19099e7b804
LeonardA-L <leonard@allain-launay.fr>
2019-08-03 09:16:39 +0200

    Import draft project

---
+++ Assets/Character.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float Speed = 0.3f;
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        _controller.Move(move * Time.deltaTime * Speed);
+    }
+
+}
