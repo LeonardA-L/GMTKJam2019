d443e0dd182dd58c935bf22e5cf9cf8087741e7e
LeonardA-L <leonard@allain-launay.fr>
2019-08-04 09:25:31 +0200

    Cel shader

---
+++ Assets/ProgressionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionManager : Singleton<ProgressionManager>
+{
+    private Dictionary<string, bool> m_switches = new Dictionary<string, bool>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_switches.Clear();
+    }
+
+    public void Set(string name, bool value)
+    {
+        m_switches[name] = value;
+    }
+
+    public bool Get(string name)
+    {
+        bool value = false;
+        m_switches.TryGetValue(name, out value);
+        return value;
+    }
+}
