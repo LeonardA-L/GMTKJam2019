86c27585616414be32ba40b0196d7469eede60d2
LeonardA-L <leonard@allain-launay.fr>
2019-08-09 20:18:50 +0200

    Import AudioManager

---
+++ Assets/AudioManager/Demo/CubeController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // Will be called when the user clicks on the cube
+    void OnMouseDown ()
+    {
+        Debug.Log("Clicked on cube, playing sound");
+
+        // Call AudioManager
+        AudioManager.Instance.PlaySound("soundName");
+    }
+
+}
